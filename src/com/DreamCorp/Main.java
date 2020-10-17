b2ae1643b31444f2768898f20ca9f20c98aceb2f
melitamariah <72455247+melitamariah@users.noreply.github.com>
2020-10-17 11:12:30 -0700

    <commit_message>

--- src/com/DreamCorp/Main.java
+++ src/com/DreamCorp/Main.java
@@ -10,19 +10,27 @@ public class Main {
 
         double firstNumber;
         double secondNumber;
-        double answer = 0.0;
+        double answer;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Dream Corp Calculator!");
-        System.out.print("Enter first number: ");
-        firstNumber = scanner.nextDouble();
-
-        System.out.print("Enter second number: ");
-        secondNumber = scanner.nextDouble();
 
         System.out.print("Choose an operand: ");
         char guestOperand = scanner.next().charAt(0);
+        if (guestOperand == '!') {
+            System.out.print("Enter a number: ");
+            firstNumber = scanner.nextDouble();
+            secondNumber = 0;
+        } else {
+
+            System.out.print("Enter first number: ");
+            firstNumber = scanner.nextDouble();
+
+            System.out.print("Enter second number: ");
+            secondNumber = scanner.nextDouble();
+        }
+
 
     /*   switch (guestOperand) {
 
@@ -45,23 +53,37 @@ public class Main {
             System.out.println(firstNumber + " " + guestOperand + " " + secondNumber + " = " + answer);
             System.out.println("Total: " + answer);
 
-        } else if (guestOperand == '+') {
+        }
+        else if (guestOperand == '+') {
             answer = firstNumber + secondNumber;
             System.out.println(firstNumber + " " + guestOperand + " " + secondNumber + " = " + answer);
             System.out.println("Total: " + answer);
 
-        } else if (guestOperand == '-') {
+        }
+        else if (guestOperand == '-') {
             answer = firstNumber - secondNumber;
             System.out.println(firstNumber + " " + guestOperand + " " + secondNumber + " = " + answer);
             System.out.println("Total: " + answer);
 
-        } else if (secondNumber == 0 && guestOperand == '/') {
-            System.err.println("Enter value greater than 0.");
+        }
+        else if (secondNumber == 0 && guestOperand == '/') {
+            System.err.println("Cannot divide by 0. Enter a value greater than zero.");
         }
         else if (guestOperand == '/' && secondNumber != 0) {
             answer = firstNumber / secondNumber;
             System.out.println(firstNumber + " " + guestOperand + " " + secondNumber + " = " + answer);
             System.out.println("Total: " + answer);
+        }
+        else if (guestOperand == '^') {
+            answer = (Math.pow(firstNumber, secondNumber));
+            System.out.println(answer);
+        } else if (guestOperand == '!') {
+            answer = 1;
+            for(int i = 1; i <= firstNumber; ++i) {
+                answer *= i;
+            }
+                System.out.println(answer);
+
         }
     }
 }
